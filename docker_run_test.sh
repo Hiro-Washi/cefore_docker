d352eaa827a4835f3de5566e27271d49727eafd0
Hiroto <c1115332@planet.kanazawa-it.ac.jp>
2025-06-11 17:02:19 +0900

    f

---
+++ docker_run_test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+docker run -it -d -w /app -v ./:/docker -h "test" --name "ceftest2" -p 9890:9896 linuxcontainers/debian-slim 
+
+(cd tmp)
+
+apt update
+apt upgrade
+apt install -y libssh-dev make automake build-essential procps zip
+
+cd /tmp
+unzip /docker/v0.11.0.zip -d ./
+cd cefore-0.11.0
+export CEFORE_DIR=/usr/local
+aclocal
+automake
+./configure --enable-csmgr --enable-cache
+make
+sudo make install
+
+(echo "include $CEFORE_DIR/lib">> /etc/ld.so.conf)
+ldconfig
+
+sh /docker/init.sh 
+# Unable to start csmgrd, Illegal CS_MODE=0
+
+
